72b0d178a096f640f14cc51794c7813067a8daf0
vtraag <vincent@traag.net>
2018-10-30 22:09:04 +0100

    Updated documentation, also updated release script to take into account the version as determined by git describe.

--- release.sh
+++ release.sh
@@ -3,17 +3,21 @@ VERSION=`git describe --tags`
 printf "Building for version $VERSION\n"
 printf "Make sure versions match also in documentation and source code.\n"
 
+if [ ! -d "build/jar" ]; then
+  mkdir build/jar
+fi
+
 printf "\nBuilding java class files\n"
 printf "================================================\n"
 javac -d build/jar src/cwts/networkanalysis/*.java src/cwts/networkanalysis/run/*.java src/cwts/util/*.java
 
 printf "\nPackaging jar file\n"
 printf "================================================\n"
-jar cvfe dist/RunNetworkClustering.jar cwts.networkanalysis.run.RunNetworkClustering -C build/jar .
+jar cvfe dist/RunNetworkClustering-$VERSION.jar cwts.networkanalysis.run.RunNetworkClustering -C build/jar .
 
 printf "\nPackaging source code\n"
 printf "================================================\n"
-find src -type f -iname "*.java" -print0 | tar -czvf dist/RunNetworkClustering-source.tar.gz --null -T -
+find src -type f -iname "*.java" -print0 | tar -czvf dist/RunNetworkClustering-$VERSION-source.tar.gz --null -T -
 
 printf "\nBuilding javadoc\n"
 printf "================================================\n"
