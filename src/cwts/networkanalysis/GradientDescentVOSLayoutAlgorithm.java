0c83ff888a0fa5f28685fc38558cbd5ea78598a3
Nees Jan van Eck <36573334+neesjanvaneck@users.noreply.github.com>
2020-04-23 19:49:30 +0200

    Implement improveLayout method

--- src/cwts/networkanalysis/GradientDescentVOSLayoutAlgorithm.java
+++ src/cwts/networkanalysis/GradientDescentVOSLayoutAlgorithm.java
@@ -1,6 +1,9 @@
 package cwts.networkanalysis;
 
+import java.util.Arrays;
 import java.util.Random;
+
+import cwts.util.FastMath;
 
 /**
  * GradientDescentVOSLayoutAlgorithm
@@ -75,6 +78,124 @@ public class GradientDescentVOSLayoutAlgorithm extends VOSLayoutAlgorithm implem
 
     public void improveLayout(Network network, Layout layout)
     {
+        boolean[] nodeVisited;
+        double a, b, distance, distance1, distance2, gradient1, gradient2, gradientLength, qualityFunction,
+                qualityFunctionOld, squaredDistance, stepLength;
+        int i, j, k, l, nQualityFunctionImprovements;
+        int[] nodePermutation;
 
+        nodePermutation = cwts.util.Arrays.generateRandomPermutation(network.nNodes, random);
+
+        stepLength = initialStepLength;
+        qualityFunction = Double.POSITIVE_INFINITY;
+        nQualityFunctionImprovements = 0;
+        nodeVisited = new boolean[network.nNodes];
+        i = 0;
+        while ((i < maxNIterations) && (stepLength >= minStepLength))
+        {
+            qualityFunctionOld = qualityFunction;
+            qualityFunction = 0;
+            Arrays.fill(nodeVisited, false);
+            for (j = 0; j < network.nNodes; j++)
+            {
+                k = nodePermutation[j];
+
+                gradient1 = 0;
+                gradient2 = 0;
+
+                for (l = network.firstNeighborIndices[k]; l < network.firstNeighborIndices[k + 1]; l++)
+                {
+                    distance1 = layout.coordinates[0][k] - layout.coordinates[0][network.neighbors[l]];
+                    distance2 = layout.coordinates[1][k] - layout.coordinates[1][network.neighbors[l]];
+                    squaredDistance = distance1 * distance1 + distance2 * distance2;
+
+                    distance = Math.sqrt(squaredDistance);
+                    a = FastMath.fastPow(distance, attraction);
+
+                    if (squaredDistance > 0)
+                    {
+                        b = network.edgeWeights[l] * a / squaredDistance;
+                        gradient1 += b * distance1;
+                        gradient2 += b * distance2;
+                    }
+
+                    if (!nodeVisited[network.neighbors[l]])
+                        if (attraction != 0)
+                            qualityFunction += network.edgeWeights[l] * a / attraction;
+                        else
+                            qualityFunction += network.edgeWeights[l] * Math.log(distance);
+                }
+
+                for (l = 0; l < network.nNodes; l++)
+                    if (l != k)
+                    {
+                        distance1 = layout.coordinates[0][k] - layout.coordinates[0][l];
+                        distance2 = layout.coordinates[1][k] - layout.coordinates[1][l];
+                        squaredDistance = distance1 * distance1 + distance2 * distance2;
+                        distance = Math.sqrt(squaredDistance);
+                        a = FastMath.fastPow(distance, repulsion);
+
+                        if (squaredDistance > 0)
+                        {
+                            b = network.nodeWeights[k] * network.nodeWeights[l] * a / squaredDistance;
+                            gradient1 -= b * distance1;
+                            gradient2 -= b * distance2;
+                        }
+
+                        if (!nodeVisited[l])
+                            if (repulsion != 0)
+                                qualityFunction -= network.nodeWeights[k] * network.nodeWeights[l] * a / repulsion;
+                            else
+                                qualityFunction -= network.nodeWeights[k] * network.nodeWeights[l] * Math.log(distance);
+                    }
+
+                if (edgeWeightIncrement > 0)
+                    for (l = 0; l < network.nNodes; l++)
+                        if (l != k)
+                        {
+                            distance1 = layout.coordinates[0][k] - layout.coordinates[0][l];
+                            distance2 = layout.coordinates[1][k] - layout.coordinates[1][l];
+                            squaredDistance = distance1 * distance1 + distance2 * distance2;
+                            distance = Math.sqrt(squaredDistance);
+                            a = FastMath.fastPow(distance, attraction);
+
+                            if (squaredDistance > 0)
+                            {
+                                b = edgeWeightIncrement * a / squaredDistance;
+                                gradient1 += b * distance1;
+                                gradient2 += b * distance2;
+                            }
+
+                            if (!nodeVisited[l])
+                                if (attraction != 0)
+                                    qualityFunction += edgeWeightIncrement * a / attraction;
+                                else
+                                    qualityFunction += edgeWeightIncrement * Math.log(distance);
+                        }
+
+                gradientLength = Math.sqrt(gradient1 * gradient1 + gradient2 * gradient2);
+                layout.coordinates[0][k] -= stepLength * gradient1 / gradientLength;
+                layout.coordinates[1][k] -= stepLength * gradient2 / gradientLength;
+
+                nodeVisited[k] = true;
+            }
+
+            if (qualityFunction < qualityFunctionOld)
+            {
+                nQualityFunctionImprovements++;
+                if (nQualityFunctionImprovements >= requiredNQualityFunctionImprovements)
+                {
+                    stepLength /= stepLengthReduction;
+                    nQualityFunctionImprovements = 0;
+                }
+            }
+            else
+            {
+                stepLength *= stepLengthReduction;
+                nQualityFunctionImprovements = 0;
+            }
+
+            i++;
+        }
     }
 }
