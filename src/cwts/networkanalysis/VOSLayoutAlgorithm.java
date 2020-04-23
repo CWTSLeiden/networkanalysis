34ce6d18e466523a23aad947bfaf30548736b163
Nees Jan van Eck <36573334+neesjanvaneck@users.noreply.github.com>
2020-04-23 19:48:31 +0200

    Implement calcQuality method

--- src/cwts/networkanalysis/VOSLayoutAlgorithm.java
+++ src/cwts/networkanalysis/VOSLayoutAlgorithm.java
@@ -1,5 +1,7 @@
 package cwts.networkanalysis;
 
+import cwts.util.FastMath;
+
 /**
  * VOSLayoutAlgorithm
  *
@@ -42,6 +44,50 @@ public abstract class VOSLayoutAlgorithm implements Cloneable, QualityLayoutAlgo
 
     public double calcQuality(Network network, Layout layout)
     {
-        return 0;
+        double distance, distance1, distance2, qualityFunction;
+        int i, j;
+
+        qualityFunction = 0;
+
+        for (i = 0; i < network.nNodes; i++)
+            for (j = network.firstNeighborIndices[i]; j < network.firstNeighborIndices[i + 1]; j++)
+                if (network.neighbors[j] < i)
+                {
+                    distance1 = layout.coordinates[0][i] - layout.coordinates[0][network.neighbors[j]];
+                    distance2 = layout.coordinates[1][i] - layout.coordinates[1][network.neighbors[j]];
+                    distance = Math.sqrt(distance1 * distance1 + distance2 * distance2);
+                    if (attraction != 0)
+                        qualityFunction += network.edgeWeights[j] * FastMath.fastPow(distance, attraction) / attraction;
+                    else
+                        qualityFunction += network.edgeWeights[j] * Math.log(distance);
+                }
+
+        for (i = 0; i < network.nNodes; i++)
+            for (j = 0; j < i; j++)
+            {
+                distance1 = layout.coordinates[0][i] - layout.coordinates[0][j];
+                distance2 = layout.coordinates[1][i] - layout.coordinates[1][j];
+                distance = Math.sqrt(distance1 * distance1 + distance2 * distance2);
+                if (repulsion != 0)
+                    qualityFunction -= network.nodeWeights[i] * network.nodeWeights[j]
+                            * FastMath.fastPow(distance, repulsion) / repulsion;
+                else
+                    qualityFunction -= network.nodeWeights[i] * network.nodeWeights[j] * Math.log(distance);
+            }
+
+        if (edgeWeightIncrement > 0)
+            for (i = 0; i < network.nNodes; i++)
+                for (j = 0; j < i; j++)
+                {
+                    distance1 = layout.coordinates[0][i] - layout.coordinates[0][j];
+                    distance2 = layout.coordinates[1][i] - layout.coordinates[1][j];
+                    distance = Math.sqrt(distance1 * distance1 + distance2 * distance2);
+                    if (attraction != 0)
+                        qualityFunction += edgeWeightIncrement * FastMath.fastPow(distance, attraction) / attraction;
+                    else
+                        qualityFunction += edgeWeightIncrement * Math.log(distance);
+                }
+
+        return qualityFunction;
     }
 }
