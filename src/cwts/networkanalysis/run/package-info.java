cb634dfdf9a32a260931c8c9b6b7f86f73e744fa
Nees Jan van Eck <36573334+neesjanvaneck@users.noreply.github.com>
2020-05-08 21:21:09 +0200

    Update package info

--- src/cwts/networkanalysis/run/package-info.java
+++ src/cwts/networkanalysis/run/package-info.java
@@ -2,11 +2,6 @@
  * Provides classes for running network analysis algorithms.
  *
  * <p>
- * Currently, the focus of this package is restricted to clustering algorithms
- * (also known as community detection algorithms).
- * </p>
- *
- * <p>
  * The network analysis algorithms are provided by the package {@link
  * cwts.networkanalysis}.
  * </p>
